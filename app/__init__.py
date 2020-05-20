6762783d7dea9c6e31582988cda580c9b80d8f8d
kusiakpgs <kusiakpgs@gmail.com>
2020-05-20 16:28:17 +0200

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from app.api import bp as api_bp
 
 from config import Config
-
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'you-will-never-guess'
@@ -13,9 +11,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-app.register_blueprint(api_bp, url_prefix='/api')
-
 login = LoginManager(app)
 login.login_view = 'login'
+
+from app.api import bp as api_bp
+app.register_blueprint(api_bp, url_prefix='/api')
 
 from app import routes, models
