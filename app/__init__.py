d588499a7f348c3d8c3ccba4cb6aa4cf7ddbeab4
kusiakpgs <kusiakpgs@gmail.com>
2020-06-16 13:23:52 +0200

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+import logging
 
 from config import Config
 
@@ -18,3 +23,17 @@ from app.api import bp as api_bp
 app.register_blueprint(api_bp, url_prefix='/db/api')
 
 from app import routes, models
+
+if not app.debug:
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
