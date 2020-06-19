6784e93234ad0953b414a53fc79882a6bcc9815a
kusiakpgs <kusiakpgs@gmail.com>
2020-06-19 15:20:26 +0200

    fix for db  path

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DB_ENDPOINT = os.environ.get('DB_ENDPOINT') or 'error'
 
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #                           'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:adminadmin@' + os.environ.get('DB_ENDPOINT') + ':3306/tkusiakdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:adminadmin@' + DB_ENDPOINT + ':3306/tkusiakdb'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
